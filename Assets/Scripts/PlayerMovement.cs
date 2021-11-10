72dec3d69bc63ac547bd0b2d89db4bf8abc7205c
gerranR <60340232+gerranR@users.noreply.github.com>
2021-11-10 10:44:39 +0100

    wall jump fix 2.0

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     public bool hasKnockback, wallJumpCheck;
     public LayerMask groundLayer;
     public PhysicsMaterial2D playerMat;
+    private bool canMove = true;
 
     private void Awake()
     {
@@ -26,31 +27,35 @@ public class PlayerMovement : MonoBehaviour
 
     void movement()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
+        if (canMove)
         {
-            rigidbody2d.velocity = new Vector2((speed / resistance) * Time.deltaTime, rigidbody2d.velocity.y);
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                rigidbody2d.velocity = new Vector2((speed / resistance) * Time.deltaTime, rigidbody2d.velocity.y);
+            }
+            else if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                rigidbody2d.velocity = new Vector2((-speed / resistance) * Time.deltaTime, rigidbody2d.velocity.y);
+            }
+            else if(!hasKnockback)
+            {
+                rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+            }
         }
-        else if(Input.GetAxisRaw("Horizontal") != 0)
-        {
-            rigidbody2d.velocity = new Vector2((-speed / resistance) * Time.deltaTime, rigidbody2d.velocity.y);
-        }
-        else
-        {
-            turnMovement(false);
-        }
-
         if (Input.GetButtonDown("Jump") && jumpsLeft > 0)
         {
             if (wallJumpCheck)
             {
+                TurnMovement(false);
                 if(lorRWall == 0)
                 {
-                    rigidbody2d.velocity = new Vector2(50, jumpForce);
+                    rigidbody2d.velocity = new Vector2(5, jumpForce);
                 }
                 else if(lorRWall == 1)
                 {
-                    rigidbody2d.velocity = new Vector2(-50, jumpForce);
+                    rigidbody2d.velocity = new Vector2(-5, jumpForce);
                 }
+                StartCoroutine(WallJumpTimer());
                 jumpsLeft -= 1;
                 wallJumpCheck = false;
             }
@@ -65,6 +70,12 @@ public class PlayerMovement : MonoBehaviour
             if(rigidbody2d.velocity.y >= -1)
                 rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, -1);
         }
+    }
+
+    IEnumerator WallJumpTimer()
+    {
+        yield return new WaitForSeconds(0.2f);
+        TurnMovement(true);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -90,7 +101,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-
         if(collision.transform.tag == "Wall")
         {
             if(rigidbody2d.velocity.y <= 0 && Input.GetAxisRaw("Horizontal") != 0)
@@ -98,13 +108,18 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void turnMovement(bool canMovement)
+    public void TurnMovement(bool canMovement)
     {
         if(!hasKnockback)
         {
             if(!canMovement)
             {
+                canMove = canMovement;
                 rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+            }
+            else
+            {
+                canMove = canMovement;
             }
         }
     }
