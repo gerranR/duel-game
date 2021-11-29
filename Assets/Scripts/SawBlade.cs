09ce6e28088e2d202f3b503d4f962c036ddc0a96
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2021-11-29 15:31:09 +0100

    audio start & 3 levels

--- Assets/Scripts/SawBlade.cs
+++ Assets/Scripts/SawBlade.cs
@@ -14,6 +14,6 @@ public class SawBlade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, speed);
+        transform.Rotate(0, 0, speed * Time.deltaTime);
     }
 }
