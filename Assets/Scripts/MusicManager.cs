05c635289f55405070a5cffe2dc865d95bdd9589
gerranR <60340232+gerranR@users.noreply.github.com>
2021-12-12 19:36:46 +0100

    bgMusic

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] bgMusic;
+    public AudioSource musicSource;
+
+    private void Start()
+    {
+        StartRandomMusic();
+    }
+
+    public void StartRandomMusic()
+    {
+        var curClip = Random.Range(0, bgMusic.Length);
+        musicSource.clip = bgMusic[curClip];
+        musicSource.Play();
+    }
+}
