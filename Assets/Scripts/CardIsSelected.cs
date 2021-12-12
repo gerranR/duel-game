be24ef7f78d7bcc2fa1835bd53197282834614c5
gerranR <60340232+gerranR@users.noreply.github.com>
2021-12-12 16:41:59 +0100

    card BorderWorks

---
+++ Assets/Scripts/CardIsSelected.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardIsSelected : MonoBehaviour
+{
+    public GameObject border;
+
+    public void BorderOn()
+    {
+        border.SetActive(true);
+    }
+    public void BorderOff()
+    {
+        border.SetActive(false);
+    }
+}
