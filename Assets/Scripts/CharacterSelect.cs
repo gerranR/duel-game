4fd063ec21e7be67455ca1ce615c2007a7380e4d
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2022-01-14 13:53:31 +0100

    firehair toggle & custom cursor

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -9,7 +9,7 @@ public class CharacterSelect : MonoBehaviour
     public Animator playerAnim, armAnim;
     public int ChosenPlayer;
     public PlayerVariant[] playerVariants;
-    private GameObject armPos1Object, armPos2Object, armPivotObject;
+    private GameObject armPos1Object, armPos2Object, armPivotObject,fireHair;
     public Vector3 armPos1, armPos2, foxArmPos1, foxArmPos2;
     public Image characterSpriteImage;
     // Start is called before the first frame update
@@ -25,6 +25,7 @@ public class CharacterSelect : MonoBehaviour
     void Start()
     {
         ChosenPlayer = 0;
+        fireHair = playerAnim.gameObject.transform.Find("Firehair").gameObject;
     }
 
     // Update is called once per frame
@@ -72,12 +73,14 @@ public class CharacterSelect : MonoBehaviour
             armPivotObject.transform.localPosition = foxArmPos1;
             armPos1Object.transform.localPosition = foxArmPos1;
             armPos2Object.transform.localPosition = foxArmPos2;
+            fireHair.SetActive(false);
         }
         else
         {
             armPivotObject.transform.localPosition = armPos1;
             armPos1Object.transform.localPosition = armPos1;
             armPos2Object.transform.localPosition = armPos2;
+            fireHair.SetActive(true);
         }
     }
 
